d68014226f9c9240965e7776ec356b5bc4751935
Narrara | Nahara <nmelomoura@binghamton.edu>
2024-09-01 21:53:07 -0400

    add dataset + create dataframe from Kaggle dataset

---
+++ epl_player_stats_2023.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Loading Kaggle Dataset by creating a dataframe
+# https://www.kaggle.com/datasets/orkunaktas/premier-league-all-players-stats-2324
+df = pd.read_csv('epl_stats.csv')
+
+
+
